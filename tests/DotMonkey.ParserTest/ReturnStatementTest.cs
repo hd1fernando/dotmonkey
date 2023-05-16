274c05d8aad313b836685c3dc6ac6c950a0dc3c1
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2023-05-15 21:20:32 -0300

    test: return statement

---
+++ tests/DotMonkey.ParserTest/ReturnStatementTest.cs
@@ -0,0 +1,23 @@
+﻿using DotMonkey.LexicalAnalizer;
+using DotMonkey.Parser.AST;
+using FluentAssertions;
+using Xunit;
+
+namespace DotMonkey.ParserTest;
+
+public class ReturnStatementTest
+{
+    [Theory(DisplayName = "Let statement should work")]
+    [InlineData("return 5;")]
+    [InlineData(" return add(15);")]
+    public void Test1(string code)
+    {
+        var lexer = new Lexer(code);
+        var parser = new Parser.AST.Parser(lexer);
+
+        Program result = parser.ParserProgram();
+
+        result.Should().NotBeNull();
+        result.TokenLiteral().Should().BeEquivalentTo("return");
+    }
+}
