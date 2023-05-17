26e1cabe2cd6b4b1dbdaa383738faf7ad56174ba
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2023-05-16 21:05:13 -0300

    test: test identifier expression

---
+++ tests/DotMonkey.ParserTest/IdentifierExpressionTest.cs
@@ -0,0 +1,27 @@
+﻿using DotMonkey.LexicalAnalizer;
+using DotMonkey.Parser.AST;
+using DotMonkey.Parser.AST.Interfaces;
+using FluentAssertions;
+using Xunit;
+
+namespace DotMonkey.ParserTest;
+
+public class IdentifierExpressionTest
+{
+    [Theory]
+    [InlineData("foobar")]
+    public void Test(string input)
+    {
+        var lexer = new Lexer(input);
+        var parser = new Parser.AST.Parser(lexer);
+
+        var result = parser.ParserProgram();
+
+        parser.Errors.Should().BeEmpty();
+        result.Statements.Should().HaveCount(1);
+        IStatement identifier = result.Statements[0];
+        identifier.Should().BeOfType<Identifier>();
+        identifier.TokenLiteral().Should().Be(input);
+        (identifier as Identifier).Value.Should().Be(input);
+    }
+}
