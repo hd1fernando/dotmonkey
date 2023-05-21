53539f44e7f4055d641882bf69d03c09cca4ec55
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2023-05-21 16:04:01 -0300

    feat: create int objects

---
+++ src/DotMonkey.Parser/Object/IObjectType.cs
@@ -0,0 +1,41 @@
+﻿namespace DotMonkey.Parser.Object;
+
+
+public struct ObjectType
+{
+    public string Value { get; private set; }
+
+    private ObjectType(string value)
+    {
+        Value = value;
+    }
+
+    public static implicit operator ObjectType(string value) => new(value);
+
+    public const string INTERGER_OBJ = "INTERGER";
+}
+
+public interface IObject
+{
+    public ObjectType Type();
+    public string Inspect();
+}
+
+public struct Interger : IObject
+{
+    public long Value { get; private set; }
+
+    public Interger(long value)
+    {
+        Value = value;
+    }
+
+    public static implicit operator Interger(long value) => new(value);
+
+    public string Inspect() => Value.ToString();
+
+    public ObjectType Type()
+    {
+        return ObjectType.INTERGER_OBJ;
+    }
+}
