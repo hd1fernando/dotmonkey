c67a4cadee04aa1701bcc955eec8246a5f5d4502
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2023-05-22 20:32:03 -0300

    test: eval infix expression

--- tests/DotMonkey.ParserTest/EvaluatorTest.cs
+++ tests/DotMonkey.ParserTest/EvaluatorTest.cs
@@ -14,6 +14,17 @@ public class EvaluatorTest
     [InlineData("10", 10)]
     [InlineData("-5", -5)]
     [InlineData("-10", -10)]
+    [InlineData("5 + 5 + 5 + 5 - 10", 10)]
+    [InlineData("2 * 2 * 2 * 2 * 2", 32)]
+    [InlineData("-50 + 100 + -50", 0)]
+    [InlineData("5 * 2 + 10", 20)]
+    [InlineData("5 + 2 * 10", 25)]
+    [InlineData("20 + 2 * -10", 0)]
+    [InlineData("50 / 2 * 2 + 10", 60)]
+    [InlineData("2 * (5 + 10)", 30)]
+    [InlineData("3 * 3 * 3 + 10", 37)]
+    [InlineData("3 * (3 * 3) + 10", 37)]
+    [InlineData("(5 + 10 * 2 + 15 / 3) * 2 + -10", 50)]
     public void Test1(string input, int expected)
     {
         var evaluated = TestEval(input);
@@ -22,9 +33,26 @@ public class EvaluatorTest
     }
 
 
-    [Theory(DisplayName = "Eval Boolean objects")]
+    [Theory(DisplayName = "Eval Boolean expression")]
     [InlineData("true", true)]
     [InlineData("false", false)]
+    [InlineData("1 < 2", true)]
+    [InlineData("1 > 2", false)]
+    [InlineData("1 < 1", false)]
+    [InlineData("1 > 1", false)]
+    [InlineData("1 == 1", true)]
+    [InlineData("1 != 1", false)]
+    [InlineData("1 == 2", false)]
+    [InlineData("1 != 2", true)]
+    [InlineData("true == true", true)]
+    [InlineData("false == false", true)]
+    [InlineData("true == false", false)]
+    [InlineData("true != false", true)]
+    [InlineData("false != true", true)]
+    [InlineData("(1 < 2) == true", true)]
+    [InlineData("(1 < 2) == false", false)]
+    [InlineData("(1 > 2) == true", false)]
+    [InlineData("(1 > 2) == false", true)]
     public void Test2(string input, bool expected)
     {
         var evaluated = TestEval(input);
