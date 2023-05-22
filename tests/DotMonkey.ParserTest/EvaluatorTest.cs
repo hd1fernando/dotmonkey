6035bc639a709f8b2dca3dd6f56c571cc1314745
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2023-05-21 22:58:24 -0300

    test: evaluate integers

---
+++ tests/DotMonkey.ParserTest/EvaluatorTest.cs
@@ -0,0 +1,40 @@
+﻿using DotMonkey.LexicalAnalizer;
+using DotMonkey.Parser.AST;
+using DotMonkey.Parser.Eval;
+using DotMonkey.Parser.Object;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace DotMonkey.ParserTest;
+
+public class EvaluatorTest
+{
+    [Theory(DisplayName = "Eval integer expresson")]
+    [InlineData("5", 5)]
+    [InlineData("10", 10)]
+    public void Test1(string input, int expected)
+    {
+        var evaluated = TestEval(input);
+
+        TestIntergerObject(evaluated, expected);
+    }
+
+    private void TestIntergerObject(IObject @object, int expected)
+    {
+        @object.Should().BeOfType<Integer>();
+        var result = (Integer)@object;
+        result.Value.Should().Be(expected);
+    }
+
+    public IObject TestEval(string input)
+    {
+        var lexer = new Lexer(input);
+        var parser = new Parser.AST.Parser(lexer);
+
+        Program program = parser.ParserProgram();
+
+        return new Evaluator().Eval(program);
+    }
+
+}
