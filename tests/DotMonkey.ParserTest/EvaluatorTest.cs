7163076b6c2d0ab2c0cfd25cf7e512f072d8b224
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2023-05-21 23:26:07 -0300

    test: evaluate boolean

--- tests/DotMonkey.ParserTest/EvaluatorTest.cs
+++ tests/DotMonkey.ParserTest/EvaluatorTest.cs
@@ -5,12 +5,13 @@ using DotMonkey.Parser.Object;
 using FluentAssertions;
 using System;
 using Xunit;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace DotMonkey.ParserTest;
 
 public class EvaluatorTest
 {
-    [Theory(DisplayName = "Eval integer expresson")]
+    [Theory(DisplayName = "Eval integer objects")]
     [InlineData("5", 5)]
     [InlineData("10", 10)]
     public void Test1(string input, int expected)
@@ -18,6 +19,24 @@ public class EvaluatorTest
         var evaluated = TestEval(input);
 
         TestIntergerObject(evaluated, expected);
+    }
+
+
+    [Theory(DisplayName = "Eval Boolean objects")]
+    [InlineData("true", true)]
+    [InlineData("false", false)]
+    public void Test2(string input, bool expected)
+    {
+        var evaluated = TestEval(input);
+
+        TestIBooleanObject(evaluated, expected);
+    }
+
+    private void TestIBooleanObject(IObject @object, bool expected)
+    {
+        @object.Should().BeOfType<_Boolean>();
+        var result = (_Boolean)@object;
+        result.Value.Should().Be(expected);
     }
 
     private void TestIntergerObject(IObject @object, int expected)
